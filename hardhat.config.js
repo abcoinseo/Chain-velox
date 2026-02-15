de5ee535f3e1bbc2f4d17c46cdecf3dfe9922412
abcoinseo <abcoinseo@gmail.com>
2026-02-14 22:04:24 -0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,35 +1,10 @@
-require("hardhat");
-
 module.exports = {
-  solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      },
-      viaIR: true
-    }
-  },
-
   networks: {
     hardhat: {
       chainId: 185912,
 
-      // 🔥 High Block Capacity
-      blockGasLimit: 1000000000,   // 1 Billion
-
-      gas: "auto",
-      gasPrice: 0,
-
-      allowUnlimitedContractSize: true,
-
-      mining: {
-        auto: true,
-        interval: 0
-      },
-
-      initialBaseFeePerGas: 0
+  
+      blockGasLimit: 1000000000
     }
   }
 };
